e8c0eea4aac75f714ccc1b0026fa8577fbc829af
Gustavo Molinari <info@beetlegius.com.ar>
2025-01-19 15:46:43 -0300

    [FS-3] Configure system tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,8 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'support/factory_bot'
 require 'support/shoulda'
+require 'support/system_tests'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
