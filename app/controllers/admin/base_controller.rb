bcaaa81f88733ef0166c9987ada8301c60ce02e7
Gustavo Molinari <info@beetlegius.com.ar>
2025-01-21 16:17:32 -0300

    [FS-7] Namespace admin policies

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -5,5 +5,13 @@ module Admin
 
     before_action :authenticate_user!
     after_action :verify_authorized
+
+    def policy_scope(scope)
+      super([ :admin, scope ])
+    end
+
+    def authorize(record, query = nil)
+      super([ :admin, record ], query)
+    end
   end
 end
